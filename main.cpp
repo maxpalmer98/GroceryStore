9211b7bb8d8020d32561741de5f52f91eb93f801
maxpalmer98 <35712496+maxpalmer98@users.noreply.github.com>
2018-02-27 22:32:58 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-//Author: Maxwell Palmer
 #include<iostream>
 #include<string>
 using namespace std;
@@ -9,29 +8,42 @@ int main()
   int numItems = 0;
   char input;
   string item;
+
   do
   {
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
-  
-    if ((input == 'A'||input == 'a')&& numItems <= 4)
+    cout<<"\n==GROCERY LIST MANAGER==";
+    cout<<"\nEnter your choice: ";
+    cout<<"\n (A)dd an item";
+    cout<<"\n (Q)uit";
+    cout<<"\nYour choice (A/Q): ";
+    cin>>input;
+
+    if (input == 'A'||input == 'a')
     {
-    cout<<"What is the item?"<<endl;
-    cin>>item;
-    list[numItems] = item;
-    numItems++;
+      cout<<"What is the item?"<<endl;
+      if(numItems < 5)
+      {
+        cin>>item;
+        list[numItems] = item;
+        numItems++;
+      }
+      else if (numItems >= 5&&(input == 'a'||input == 'A'))
+      {
+        cout<<"You'll need a bigger list!"<<endl;
+      }
     }
-    else if (numItems >= 5&&(input == 'a'||input == 'A')) 
-    {
-    cout<<"What is the item?"<<endl;
-    cout<<"You'll need a bigger list!"<<endl;
-    }
-    
-  }
-  while(input != 'q'&&input != 'Q');
-  return 0;
+
+   }
+   while(input != 'q'&&input != 'Q');
+
+   if(input == 'q'||input == 'Q')
+   {
+     cout<<"==ITEMS TO BUY==";
+     cout<<"\n1 "<<list[0];
+     cout<<"\n2 "<<list[1];
+     cout<<"\n3 "<<list[2];
+     cout<<"\n4 "<<list[3];
+     cout<<"\n5 "<<list[4];
+   }
+   return 0;
 }
