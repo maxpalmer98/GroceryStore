a8cdb518a0657ea284bc0ce0f8b7a40e59aefd1f
maxpalmer98 <35712496+maxpalmer98@users.noreply.github.com>
2018-03-13 11:29:49 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -22,7 +22,6 @@ int main()
     if (input == 'A'||input == 'a')
     {
       cout<<"What is the item?"<<endl;
-<<<<<<< HEAD
       cin>>item;
       list.push_back(item);
       numItems++; 
@@ -42,31 +41,4 @@ int main()
 
   
   return 0;
-=======
-      if(numItems < 5)
-      {
-        cin>>item;
-        list[numItems] = item;
-        numItems++;
-      }
-      else if (numItems >= 5&&(input == 'a'||input == 'A'))
-      {
-        cout<<"You'll need a bigger list!"<<endl;
-      }
-    }
 
-   }
-   while(input != 'q'&&input != 'Q');
-
-   if(input == 'q'||input == 'Q')
-   {
-     cout<<"==ITEMS TO BUY==";
-     cout<<"\n1 "<<list[0];
-     cout<<"\n2 "<<list[1];
-     cout<<"\n3 "<<list[2];
-     cout<<"\n4 "<<list[3];
-     cout<<"\n5 "<<list[4];
-   }
-   return 0;
->>>>>>> 9211b7bb8d8020d32561741de5f52f91eb93f801
-}
