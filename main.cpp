49b9144feaa11ba81520445bb93bc702335af980
Maxwell A Palmer <mpalmer16@mail.csuchico.edu>
2018-03-13 11:25:24 -0700

    Makes the list dynamic

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
 #include<string>
+#include<vector>
 using namespace std;
 
 int main()
 {
-  string list[5]; //array of 5 strings
+  vector<string> list; //vector for list
   int numItems = 0;
   char input;
   string item;
@@ -21,29 +22,23 @@ int main()
     if (input == 'A'||input == 'a')
     {
       cout<<"What is the item?"<<endl;
-      if(numItems < 5)
-      {
-        cin>>item;
-        list[numItems] = item;
-        numItems++;
-      }
-      else if (numItems >= 5&&(input == 'a'||input == 'A'))
-      {
-        cout<<"You'll need a bigger list!"<<endl;
-      }
+      cin>>item;
+      list.push_back(item);
+      numItems++; 
     }
-
   }
   while(input != 'q'&&input != 'Q');
 
-  if(input == 'q'||input == 'Q')
+  if (numItems == 0)
   {
-    cout<<"==ITEMS TO BUY==";
-    cout<<"\n1 "<<list[0];
-    cout<<"\n2 "<<list[1];
-    cout<<"\n3 "<<list[2];
-    cout<<"\n4 "<<list[3];
-    cout<<"\n5 "<<list[4];
+    cout<<"No items to buy!"<<endl;
   }
+  
+  for(int i = 0; i < list.size(); i++)
+  {
+    cout<<list[i]<<endl;
+  }
+
+  
   return 0;
 }
